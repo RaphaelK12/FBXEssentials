0a912ea7ea74036dc3b792708b18b245c356d71e
buttburger <butt_burger@outlook.com>
2018-01-18 17:24:55 +0800

    Update object.h

--- OpenGL3/OpenGL3/object.h
+++ OpenGL3/OpenGL3/object.h
@@ -2,30 +2,27 @@
 #define _OBJECT_H
 #define MAX_VERTICES 800000 //Max number of vertices(for each object)
 #define MAX_POLYGONS 800000 //Max number of polygons(for each object)
+#define MAX_VALUE 3000
 #define MAX_OBJECTS 100 //Max number of objects
 char name[20]; //Name of the object
 int id_texture;
 int vertices_quantity; //Number of vertices
 int polygons_quantity; //Number of polygons
-// Our vertex type
 struct vertex
 {
 	float x, y, z;
 }vertices[MAX_VERTICES], normal[MAX_VERTICES], test[MAX_VERTICES];
 //Array of vertices normals
 // The polygon(triangle), 3 numbers that aim 3 vertices
-struct
+struct polygon
 {
 	unsigned short a, b, c;
-}polygon[MAX_POLYGONS];
+}polygons[MAX_POLYGONS];
 //Array of polygons (numbers that point to the vertices' list)
 // The mapcoord type, 2 texture coordinates for each vertex
-struct
+struct map
 {
-	float u, v;
-}mapcoord[MAX_VERTICES];
-// Array of U,V coordinates for texture mapping
-
-
-
+	float u[123][3], v[123][3];
+}mapcoords;
+// Array of UV coordinates for texture mapping
 #endif
